8b5e72fd8e7e2b6d94895529bc75448d7518c263
lutzh <lutz.huehnken@altocon.de>
2011-07-27 22:09:32 +0200

    started module

--- tapestry-wurfl-demo/src/main/java/com/altocon/tapestry_wurfl/pages/Index.java
+++ tapestry-wurfl-demo/src/main/java/com/altocon/tapestry_wurfl/pages/Index.java
@@ -2,15 +2,9 @@ package com.altocon.tapestry_wurfl.pages;
 
 import java.util.Date;
 
-import javax.servlet.ServletContext;
-import javax.servlet.http.HttpServletRequest;
-
 import net.sourceforge.wurfl.core.Device;
-import net.sourceforge.wurfl.core.WURFLHolder;
-import net.sourceforge.wurfl.core.WURFLManager;
 
 import org.apache.tapestry5.ioc.annotations.Inject;
-import org.apache.tapestry5.services.ApplicationGlobals;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,27 +16,15 @@ public class Index {
     private final Logger logger = LoggerFactory.getLogger(Index.class);
 
     @Inject
-    private ApplicationGlobals _applicationGlobals;
-
-    @Inject
-    private HttpServletRequest _request;
+    private Device _device;
 
     public Date getCurrentTime() {
         return new Date();
     }
 
     public String getDeviceID() {
-        final ServletContext servletContext = _applicationGlobals.getServletContext();
+        logger.debug("Device: {}", _device.getId());
 
-        final WURFLHolder wurflHolder = (WURFLHolder) servletContext
-                .getAttribute("net.sourceforge.wurfl.core.WURFLHolder");
-
-        final WURFLManager wurfl = wurflHolder.getWURFLManager();
-
-        final Device device = wurfl.getDeviceForRequest(_request);
-
-        logger.debug("Device: {}", device.getId());
-
-        return device.getId();
+        return _device.getId();
     }
 }
